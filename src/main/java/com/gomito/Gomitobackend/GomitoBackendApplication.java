57cfe26b40aa290a62c42bc3842cc7b8ecfb4fd4
tuyentac95 <tuyentac95@gmail.com>
2020-11-03 15:03:07 +0700

    tạo dự án

---
+++ src/main/java/com/gomito/Gomitobackend/GomitoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gomito.Gomitobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomitoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GomitoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomito/Gomitobackend/GomitoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomito.Gomitobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomitoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
