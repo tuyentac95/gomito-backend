ffbe85c3825ba0d1e8d2c64778da0ce816196273
Hiep Nguyen <lamchanhiephceet@gmail.com>
2020-11-04 16:01:11 +0700

    cap nhat repo service boardList

---
+++ src/main/java/com/gomito/Gomitobackend/model/GBoard.java
@@ -0,0 +1,26 @@
+package com.gomito.Gomitobackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class GBoard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+    private String boardName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private GUser user;
+
+    @OneToMany(fetch=FetchType.LAZY)
+    private List<GList> lists;
+}
