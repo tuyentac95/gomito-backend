ef81a8e72490ae4177f04d8e1c3c4f536feafe01
Hiep Nguyen <lamchanhiephceet@gmail.com>
2020-11-05 15:24:34 +0700

    code nham tren nhanh dev

---
+++ src/main/java/com/gomito/Gomitobackend/controller/CardController.java
@@ -0,0 +1,33 @@
+package com.gomito.Gomitobackend.controller;
+
+import com.gomito.Gomitobackend.model.GCard;
+import com.gomito.Gomitobackend.model.GCardDto;
+import com.gomito.Gomitobackend.model.GList;
+import com.gomito.Gomitobackend.service.GCardService;
+import com.gomito.Gomitobackend.service.GListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cards")
+@CrossOrigin("*")
+public class CardController {
+
+    @Autowired
+    private GListService gListService;
+
+    @Autowired
+            private GCardService gCardService;
+
+    @PostMapping("/")
+        public ResponseEntity<GCard> createCard(@RequestBody GCardDto cardDto) {
+            GCard gcard = new GCard();
+            gcard.setCardName(cardDto.getCardName());
+            GList gList = gListService.findById(cardDto.getListId());
+            gcard.setList(gList);
+            GCard gCard = gCardService.save(gcard);
+            return ResponseEntity.status(HttpStatus.CREATED).body(gCard);
+        }
+}
