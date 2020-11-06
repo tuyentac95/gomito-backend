25faaae8bcf18204d505ecd66a80fe67043269f1
thanhbinh2612 <thanhbinh261294@gmail.com>
2020-11-06 22:50:18 +0700

    Vừa viết xong check list, board, user cho 1 card (check xem là card đó có đúng nằm trong list, nằm trong board, nằm trong user ko)

--- src/main/java/com/gomito/Gomitobackend/controller/CardController.java
+++ src/main/java/com/gomito/Gomitobackend/controller/CardController.java
@@ -1,8 +1,8 @@
 package com.gomito.Gomitobackend.controller;
 
-import com.gomito.Gomitobackend.model.GCard;
-import com.gomito.Gomitobackend.model.GCardDto;
-import com.gomito.Gomitobackend.model.GList;
+import com.gomito.Gomitobackend.model.*;
+import com.gomito.Gomitobackend.service.AuthService;
+import com.gomito.Gomitobackend.service.GBoardService;
 import com.gomito.Gomitobackend.service.GCardService;
 import com.gomito.Gomitobackend.service.GListService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +16,17 @@ import java.util.List;
 @RequestMapping("/api/cards")
 @CrossOrigin("*")
 public class CardController {
+    @Autowired
+    GBoardService gBoardService;
 
     @Autowired
     private GListService gListService;
 
     @Autowired
     private GCardService gCardService;
+
+    @Autowired
+    AuthService authService;
 
     @PostMapping("/")
         public ResponseEntity<GCard> createCard(@RequestBody GCardDto cardDto) {
@@ -40,25 +45,31 @@ public class CardController {
     public ResponseEntity<String> updateCardIndex(@RequestBody List<GCardDto> updateCards){
         Long listId = getListId(updateCards);
         if (listId > 0){
-            Long boardId = getBoardId(listId);
-            if(boardId > 0){
-
+            GList gList = gListService.findById(listId);
+            Long boardIdNew = gList.getBoard().getBoardId();
+            if(boardIdNew > 0) {
+                if (checkUserId(boardIdNew)) {
+                    for (GCardDto card : updateCards) {
+                        GCard gCard = gCardService.findById(card.getCardId());
+                        gCard.setCardIndex(card.getCardIndex());
+                        GCard saveCard = gCardService.save(gCard);
+                    }
+                    return ResponseEntity.status(HttpStatus.OK).body("Updated successfully!");
+                } else {
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You don't have authorization to modify!");
+                }
             }
-        }
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You don't have authorization to modify!");
         }
 
-//    private Long getBoardId(Long listId) {
-//        GCard gCard = gCardService.findById(updateCards.get(0).getCardId());
-//        Long listId = gCard.getList().getListId();
-//        for (GCardDto card: updateCards){
-//            GCard gCardNew = gCardService.findById(card.getCardId());
-//            Long listIdNew = gCardNew.getList().getListId();
-//            if (!listIdNew.equals(listId)){
-//                return -1L;
-//            }
-//        }
-//        return listId;
-//    }
+    private boolean checkUserId(Long boardIdNew) {
+        GBoard gBoard = gBoardService.findById(boardIdNew);
+        GUser checkUser = gBoard.getUser();
+        GUser authUser = authService.getCurrentUser();
+        return checkUser.getUserId().equals(authUser.getUserId());
+    }
+
 
     private Long getListId(List<GCardDto> updateCards) {
         GCard gCard = gCardService.findById(updateCards.get(0).getCardId());
