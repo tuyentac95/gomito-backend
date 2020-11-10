066e09155708879109f010b2af731ca5d6e5f6b6
tuyentac95 <tuyentac95@gmail.com>
2020-11-11 00:58:18 +0700

    test tính năng thêm member vào card

--- src/main/java/com/gomito/Gomitobackend/controller/ListController.java
+++ src/main/java/com/gomito/Gomitobackend/controller/ListController.java
@@ -4,7 +4,6 @@ import com.gomito.Gomitobackend.dto.GListDto;
 import com.gomito.Gomitobackend.model.*;
 import com.gomito.Gomitobackend.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
