ceaf5a6868b76f21d63796a0575b8d898f8e733e
tuyentac95 <tuyentac95@gmail.com>
2020-11-12 16:00:01 +0700

    commit trước khi sang nhánh mới

--- src/main/java/com/gomito/Gomitobackend/config/WebSocketConfig.java
+++ src/main/java/com/gomito/Gomitobackend/config/WebSocketConfig.java
@@ -11,14 +11,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/notification");
-        registry.setApplicationDestinationPrefixes("/users");
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/notifications")
-                .setAllowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
+                .setAllowedOrigins("*")
                 .withSockJS();
     }
 }
